a4ba0e85f54af842af3bee2c29f91ed5e6ca124c
CK Kashyap <ckkashyap@gmail.com>
2015-02-11 21:07:22 +0530

    .threadvar. only works if threads are turned on. This exposed a bug in nim ... have applied a local patch till it gets fixed

--- vm64.nim
+++ vm64.nim
@@ -1,7 +1,8 @@
 import procs
 
 #var cpu* {.codegenDecl: "__thread dingo $# $#$#".} : ptr CPU 
-var cpu*: ptr CPU 
+#var cpu* {.codegenDecl: "$# $# asm(\"%fs:0\")".} : ptr CPU 
+var cpu*{.threadvar.}: ptr CPU 
 
 
 
